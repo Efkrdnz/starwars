79df05e19487972feacca4cf57203f57a1f1e776
Efkrdnz <tahsinkaradenizformal@gmail.com>
2025-09-06 13:37:14 +0200

    TODO: block animation fix

--- src/main/java/net/efkrdnz/starwarsverse/procedures/GuardProjectilenonparryProcedure.java
+++ src/main/java/net/efkrdnz/starwarsverse/procedures/GuardProjectilenonparryProcedure.java
@@ -8,14 +8,18 @@ import net.neoforged.bus.api.ICancellableEvent;
 import net.neoforged.bus.api.Event;
 
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.component.CustomData;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.Mth;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.server.level.ServerLevel;
-
-import net.efkrdnz.starwarsverse.network.StarwarsverseModVariables;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.component.DataComponents;
 
 import javax.annotation.Nullable;
 
@@ -38,40 +42,39 @@ public class GuardProjectilenonparryProcedure {
 		if (entity == null || immediatesourceentity == null || sourceentity == null)
 			return;
 		double rand = 0;
-		if (entity.isShiftKeyDown()) {
-			if (entity.getData(StarwarsverseModVariables.PLAYER_VARIABLES).guard > 0) {
-				if (sourceentity instanceof Projectile || immediatesourceentity instanceof Projectile) {
-					if (event instanceof ICancellableEvent _cancellable) {
-						_cancellable.setCanceled(true);
+		if (entity.isShiftKeyDown() && (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("minecraft:lightsaber")))
+				&& (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getBoolean("enabled")) {
+			if (sourceentity instanceof Projectile || immediatesourceentity instanceof Projectile) {
+				if (event instanceof ICancellableEvent _cancellable) {
+					_cancellable.setCanceled(true);
+				}
+				rand = Mth.nextInt(RandomSource.create(), 1, 3);
+				if (rand == 1) {
+					AnimUtils.disableFirstPersonAnim = true;
+					if (world.isClientSide()) {
+						SetupAnimationsProcedure.setAnimationClientside((Player) entity, "block1", false);
 					}
-					rand = Mth.nextInt(RandomSource.create(), 1, 3);
-					if (rand == 1) {
-						AnimUtils.disableFirstPersonAnim = true;
-						if (world.isClientSide()) {
-							SetupAnimationsProcedure.setAnimationClientside((Player) entity, "block1", true);
-						}
-						if (!world.isClientSide()) {
-							if (entity instanceof Player)
-								PacketDistributor.sendToPlayersInDimension((ServerLevel) entity.level(), new SetupAnimationsProcedure.StarwarsverseModAnimationMessage("block1", entity.getId(), true));
-						}
-					} else if (rand == 2) {
-						AnimUtils.disableFirstPersonAnim = true;
-						if (world.isClientSide()) {
-							SetupAnimationsProcedure.setAnimationClientside((Player) entity, "block2", true);
-						}
-						if (!world.isClientSide()) {
-							if (entity instanceof Player)
-								PacketDistributor.sendToPlayersInDimension((ServerLevel) entity.level(), new SetupAnimationsProcedure.StarwarsverseModAnimationMessage("block2", entity.getId(), true));
-						}
-					} else {
-						AnimUtils.disableFirstPersonAnim = true;
-						if (world.isClientSide()) {
-							SetupAnimationsProcedure.setAnimationClientside((Player) entity, "block3", true);
-						}
-						if (!world.isClientSide()) {
-							if (entity instanceof Player)
-								PacketDistributor.sendToPlayersInDimension((ServerLevel) entity.level(), new SetupAnimationsProcedure.StarwarsverseModAnimationMessage("block3", entity.getId(), true));
-						}
+					if (!world.isClientSide()) {
+						if (entity instanceof Player)
+							PacketDistributor.sendToPlayersInDimension((ServerLevel) entity.level(), new SetupAnimationsProcedure.StarwarsverseModAnimationMessage("block1", entity.getId(), false));
+					}
+				} else if (rand == 2) {
+					AnimUtils.disableFirstPersonAnim = true;
+					if (world.isClientSide()) {
+						SetupAnimationsProcedure.setAnimationClientside((Player) entity, "block2", false);
+					}
+					if (!world.isClientSide()) {
+						if (entity instanceof Player)
+							PacketDistributor.sendToPlayersInDimension((ServerLevel) entity.level(), new SetupAnimationsProcedure.StarwarsverseModAnimationMessage("block2", entity.getId(), false));
+					}
+				} else {
+					AnimUtils.disableFirstPersonAnim = true;
+					if (world.isClientSide()) {
+						SetupAnimationsProcedure.setAnimationClientside((Player) entity, "block3", false);
+					}
+					if (!world.isClientSide()) {
+						if (entity instanceof Player)
+							PacketDistributor.sendToPlayersInDimension((ServerLevel) entity.level(), new SetupAnimationsProcedure.StarwarsverseModAnimationMessage("block3", entity.getId(), false));
 					}
 				}
 			}
